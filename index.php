87e9b87b425098fa0107691a710c3af44c48dce7
karndiy <109570596+karndiy@users.noreply.github.com>
2022-08-04 12:36:54 +0700

    update

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php 
-echo 'test aucation php \n';
+echo 'test aucation php '."<br>";
 //header( "location: http://www.ireallyhost.com" );
 // exit(0);
-echo $_SERVER['SERVER_NAME'];
+
+echo 'host: '.$_SERVER['SERVER_NAME'];
+//header( 'location: html' );
+//exit(0);
+
+
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+// If the user is on a mobile device, redirect them
+if(isMobile()){
+    header("Location: m");
+}else{
+    header("Location: html");
+}
+exit(0);
 ?>
