932cdffe1a9b815e91b7b42f9e1e8c57c1a9bfdc
karndiy <109570596+karndiy@users.noreply.github.com>
2022-08-04 13:17:43 +0700

    update vuejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'AUCATION System!',
+      title:"AUCATION System",
+      description:"ยินดีต้อนรับ เข้าสู่ระบบ ประมูล  รูปแบบใหม่ ง่าย รวดเร็ว สะดวก ปลอดภัย",
+      buttonx:'<a href="../examples/" class="btn btn-primary btn-lg px-4">เข้าสู่ระบบ</a>'
+
     }
   }
 }).mount('#app')
